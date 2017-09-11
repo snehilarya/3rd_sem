579aee9cf163adacc5cf39b7f77d8ee813c9de6b
snehil <snar16cs@cmrit.ac.in>
2017-09-11 21:19:08 +0530

    atom

---
+++ add.py
@@ -0,0 +1,8 @@
+def add(a, b): # function definition
+    return a + b
+
+
+a = float(input("enter first number :"))
+b = float(input("enter second number :"))
+sum = add(a, b)                 # function calling
+print("the addition of two number is :", sum)
